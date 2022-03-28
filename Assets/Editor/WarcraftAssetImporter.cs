d78659b9288884ee0618844e32cb7953f1c2509a
kyuuzou <newita@gmail.com>
2022-03-28 20:54:28 +0100

    Implement tool downloader as per Unity 4.0.2f1's available API.

--- Assets/Editor/WarcraftAssetImporter.cs
+++ Assets/Editor/WarcraftAssetImporter.cs
@@ -1,9 +1,15 @@
 using System;
+using System.Collections;
+using System.IO;
+using System.Threading;
 using UnityEditor;
 using UnityEngine;
 
 public class WarcraftAssetImporter : EditorWindow {
-
+	
+	private const string FFMpegURL = "https://github.com/Wargus/stratagus/releases/download/2015-30-11/ffmpeg.exe";
+	private const string WindowTitle = "Asset Importer";
+	
     [MenuItem("Window/Warcraft Asset Importer _w", false, (int)'W')]
     static void Init() {
 		// because the Inspector class is internal, we need to get it through reflection
@@ -17,13 +23,27 @@ public class WarcraftAssetImporter : EditorWindow {
         importer.Show();
     }
 	
-	private void Import() {
-		Debug.LogError("Not implemented yet!");
-	}
-	
     private void OnGUI() {
-		if (GUILayout.Button("Import")) {
-			this.Import();
-		}
+        if (GUILayout.Button("Import")) {
+            EditorUtility.DisplayProgressBar(WindowTitle, "Preparing...", 0.0f);
+			
+			string guid = AssetDatabase.CreateFolder("Assets", "Tools");
+			string toolsPath = AssetDatabase.GUIDToAssetPath(guid);
+			
+			WWW www = new WWW(FFMpegURL);
+			
+			do {
+	            EditorUtility.DisplayProgressBar(WindowTitle, "Downloading FFMpeg...", www.progress * 0.5f);
+                Thread.Sleep(200);
+			} while (! www.isDone);
+			
+			if (!string.IsNullOrEmpty(www.error)) {
+				Debug.LogError(www.error);
+			} else {
+				Debug.Log(www.size);
+			}
+			
+            EditorUtility.ClearProgressBar();
+        }
     }
 }
