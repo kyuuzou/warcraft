b35079e2243cb0b357a1ba8d782aff867fa46f9f
kyuuzou <newita@gmail.com>
2022-04-02 00:54:01 +0100

    Implement support for Warcraft assets on Windows, via War1gus' war1tool.

--- Assets/Root/Scripts/Editor/WarcraftAssetImporter.cs
+++ Assets/Root/Scripts/Editor/WarcraftAssetImporter.cs
@@ -1,14 +1,17 @@
 using System;
 using System.Collections;
+using System.Diagnostics;
 using System.IO;
-using UnityEditor;
 using Unity.EditorCoroutines.Editor;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.Networking;
 
+using Debug = UnityEngine.Debug;
+
 public class WarcraftAssetImporter : EditorWindow {
 	
-	private const string ToolsPath = "Tools";
+	private const string ToolPath = "Tools";
 	private const string WindowTitle = "Warcraft Asset Importer";
 
 	private const string ToolsURL = "https://github.com/kyuuzou/warcraft/releases/download/war1tool_v3.2.1/";
@@ -18,8 +21,8 @@ public class WarcraftAssetImporter : EditorWindow {
 	private UnityWebRequest currentRequest = null;
 	private EditorCoroutine importingCoroutine = null;
 
-    [SerializeField]
     private string dataWarPath = string.Empty;
+    private string lastOutputMessage = string.Empty;
 
     [MenuItem("Window/Warcraft Asset Importer _w", false, (int)'W')]
     private static void Init() {
@@ -64,7 +67,7 @@ public class WarcraftAssetImporter : EditorWindow {
         string progressInfo = $"Step {stepNumber}/{totalSteps}: Downloading {Path.GetFileNameWithoutExtension(url)}";
 
         do {
-            EditorUtility.DisplayProgressBar(
+            EditorUtility.DisplayCancelableProgressBar(
                 WarcraftAssetImporter.WindowTitle,
                 progressInfo,
                 this.currentRequest.downloadProgress
@@ -79,13 +82,12 @@ public class WarcraftAssetImporter : EditorWindow {
             yield break;
         }
 
-        string path = Path.Combine(Application.dataPath, WarcraftAssetImporter.ToolsPath, Path.GetFileName(url));
         File.WriteAllBytes(path, this.currentRequest.downloadHandler.data);
     }
 
     private IEnumerator DownloadTools() {
-        if (!AssetDatabase.IsValidFolder($"Assets/{WarcraftAssetImporter.ToolsPath}")) {
-            AssetDatabase.CreateFolder("Assets", WarcraftAssetImporter.ToolsPath);
+        if (!AssetDatabase.IsValidFolder($"Assets/{WarcraftAssetImporter.ToolPath}")) {
+            AssetDatabase.CreateFolder("Assets", WarcraftAssetImporter.ToolPath);
         }
 
         string[] toolFilenames = { WarcraftAssetImporter.FFMpegFilename, WarcraftAssetImporter.War1toolFilename };
@@ -98,10 +100,35 @@ public class WarcraftAssetImporter : EditorWindow {
         }
     }
 
+    private IEnumerator ExtractAssets() {
+        string path = this.dataWarPath;
+
+        if (string.IsNullOrEmpty(path) || !File.Exists(path) || string.Compare(Path.GetFileName(path), "DATA.WAR") != 0) {
+            Debug.LogError("Could not find DATA.WAR. Please select it before importing.");
+            this.Abort();
+            yield break;
+        }
+
+        //string toolPath = Application.dataPath.Replace('/', '\\') + "\\Tools\\";
+        string toolPath = $"{Application.dataPath}\\{WarcraftAssetImporter.ToolPath}\\";
+
+        // Inject the script into a command in case running scripts is disabled on the system.
+        string script = File.ReadAllText($"{toolPath}ExtractFilesOnWindows.ps1");
+        script = script.Replace("{DATA_WAR_PATH}", this.dataWarPath);
+        script = script.Replace("{TOOL_PATH}", toolPath);
+
+        string command = $"-NoExit {script}";
+
+        Process process = Process.Start("powershell.exe", command);
+        process.WaitForExit();
+        process.Close();
+    }
+
     private IEnumerator Import() {
-	    EditorUtility.DisplayProgressBar(WindowTitle, "Preparing...", 0.0f);
+	    EditorUtility.DisplayCancelableProgressBar(WindowTitle, "Preparing...", 0.0f);
 
         yield return this.DownloadTools();
+        yield return this.ExtractAssets();
 
 	    EditorUtility.ClearProgressBar();
 	    AssetDatabase.Refresh();
---
+++ Assets/Tools/ExtractFilesOnWindows.ps1
@@ -0,0 +1,5 @@
+﻿$a = New-Object -ComObject Scripting.FileSystemObject;
+$file = $a.GetFile('{DATA_WAR_PATH}')
+$folder = $a.GetParentFolderName($file.ShortPath)
+cd {TOOL_PATH}
+./war1tool.exe -v $folder ../Warcra~1/
