e9a5757b86eb933ef7fe7e43a4d52fd235bf1413
kyuuzou <newita@gmail.com>
2022-04-02 11:57:20 +0100

    Move extracted assets to the Warcraft Resources folder.

--- Assets/Tools/ExtractFilesOnWindows.ps1
+++ Assets/Tools/ExtractFilesOnWindows.ps1
@@ -2,4 +2,7 @@
 $file = $a.GetFile('{DATA_WAR_PATH}')
 $folder = $a.GetParentFolderName($file.ShortPath)
 cd {TOOL_PATH}
-./war1tool.exe -v $folder ../Warcra~1/
+./war1tool.exe -v -m $folder
+$path = '..\Warcraft Resources'
+if (Test-Path $path) { Remove-Item $path -Recurse }
+Move-Item -Path data.wc1 -Destination $path
