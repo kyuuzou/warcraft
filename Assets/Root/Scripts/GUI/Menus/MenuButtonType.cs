bd537c6b495caa167077789542161a3545334b68
kyuuzou <newita@gmail.com>
2022-04-03 16:44:59 +0100

    Fix main menu buttons.

--- Assets/Root/Scripts/GUI/Menus/MenuButtonType.cs
+++ Assets/Root/Scripts/GUI/Menus/MenuButtonType.cs
@@ -1,9 +1,10 @@
 
 public enum MenuButtonType {
-    Continue,
-    Load,
-    Quit,
-    Replay,
-    Save,
-    Start
+    None,
+    Continue    = 1,
+    Load        = 2,
+    Quit        = 3,
+    Replay      = 4,
+    Save        = 5,
+    Start       = 6
 }
