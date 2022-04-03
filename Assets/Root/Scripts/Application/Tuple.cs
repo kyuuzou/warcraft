5bc8eb6095e4f38a36a340f7e4dd9dd5bc099b8d
kyuuzou <newita@gmail.com>
2022-04-03 14:30:23 +0100

    Run formatter on Tuple.cs

--- Assets/Root/Scripts/Application/Tuple.cs
+++ Assets/Root/Scripts/Application/Tuple.cs
@@ -3,7 +3,6 @@
 // --------------------------------------------------------------------------
 
 using System;
-using System.Collections.Generic;
 
 /// <summary>
 /// Represents a functional tuple that can be used to store 
@@ -14,62 +13,62 @@ using System.Collections.Generic;
 public sealed class Tuple<T1, T2> {
     private readonly T1 item1;
     private readonly T2 item2;
-    
+
     /// <summary>
     /// Retyurns the first element of the tuple
     /// </summary>
     public T1 Item1 {
-        get { return item1; }
+        get { return this.item1; }
     }
-    
+
     /// <summary>
     /// Returns the second element of the tuple
     /// </summary>
     public T2 Item2 {
-        get { return item2; }
+        get { return this.item2; }
     }
-    
+
     /// <summary>
     /// Create a new tuple value
     /// </summary>
     /// <param name="item1">First element of the tuple</param>
     /// <param name="second">Second element of the tuple</param>
-    public Tuple (T1 item1, T2 item2) {
+    public Tuple(T1 item1, T2 item2) {
         this.item1 = item1;
         this.item2 = item2;
     }
-    
-    public override string ToString () {
-        return string.Format ("Tuple({0}, {1})", Item1, Item2);
+
+    public override string ToString() {
+        return string.Format("Tuple({0}, {1})", this.Item1, this.Item2);
     }
-    
-    public override int GetHashCode () {
+
+    public override int GetHashCode() {
         int hash = 17;
-        hash = hash * 23 + item1.GetHashCode ();
-        hash = hash * 23 + item2.GetHashCode ();
+        hash = hash * 23 + this.item1.GetHashCode();
+        hash = hash * 23 + this.item2.GetHashCode();
 
         return hash;
     }
-    
-    public override bool Equals (object o) {
-        if (o.GetType () != typeof (Tuple<T1, T2>)) {
+
+    public override bool Equals(object obj) {
+        if (obj.GetType() != typeof(Tuple<T1, T2>)) {
             return false;
         }
-        
-        var other = (Tuple<T1, T2>) o;
-        
+
+        var other = (Tuple<T1, T2>)obj;
+
         return this == other;
     }
-    
-    public static bool operator == (Tuple<T1, T2> a, Tuple<T1, T2> b) {
-        return a.item1.Equals (b.item1) && a.item2.Equals (b.item2);            
+
+    public static bool operator ==(Tuple<T1, T2> a, Tuple<T1, T2> b) {
+        return a.item1.Equals(b.item1) && a.item2.Equals(b.item2);
     }
-    
-    public static bool operator != (Tuple<T1, T2> a, Tuple<T1, T2> b) {
-        return ! (a == b);
+
+    public static bool operator !=(Tuple<T1, T2> a, Tuple<T1, T2> b) {
+        return !(a == b);
     }
-    
-    public void Unpack (Action<T1, T2> unpackerDelegate) {
-        unpackerDelegate (Item1, Item2);
+
+    public void Unpack(Action<T1, T2> unpackerDelegate) {
+        unpackerDelegate(this.Item1, this.Item2);
     }
 }
